e00a8c42103b461f85cbefecad42e22d17c322b9
Peter S <peter.sundress@gmail.com>
2026-04-06 11:39:11 +0300

    Second commit

--- webscrapping_task2.py
+++ webscrapping_task2.py
@@ -12,22 +12,27 @@ response = requests.get(url, headers=headers, timeout=10)
 response.raise_for_status()                                         #Для проверки положительного ответа
 soup = bs4.BeautifulSoup(response.text, features='lxml')
 
-
 all_articles = soup.select('article')
 for article_data in all_articles:
-    time_tag = article_data.select_one('[datetime]')
     title_tag = article_data.select_one('a.tm-title__link')
+    new_url = 'https://habr.com' + title_tag.get('href')
 
-    if not time_tag or not title_tag:
-        continue
+    response_mk2 = requests.get(new_url, headers=headers, timeout=10)
+    response_mk2.raise_for_status()
+    soup_2 = bs4.BeautifulSoup(response_mk2.text, features='lxml')
 
-    date_time_ = time_tag.get('datetime', '')[:10]
+    article = soup_2.select_one('article')
+    article_body = article.select('div')
+    article_text = ' '.join(block.get_text(' ', strip=True) for block in article_body)
+
+    time_tag = article.select_one('[datetime]')
+
     title = title_tag.get_text('span', strip=True)
-    link = 'https://habr.com' + title_tag.get('href')
-    article_text = article_data.get_text(' ', strip=True).lower()
+    date_time_ = time_tag.get('datetime', '')[:10]
 
     if any(keyword.lower() in article_text for keyword in KEYWORDS):
-        print(f'{date_time_} - {title} - {link}')
+        print(f'{date_time_} - {title} - {new_url}')
 
-if __name__ == '__main__':
-    pass
+    if __name__ == '__main__':
+        pass
+
